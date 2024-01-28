f5ea216b1b681ddfac09997782741680de5b972e
francofola <62245466+francofola@users.noreply.github.com>
2024-01-28 15:57:49 +0100

    Connected Frontend - Backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    users: []
+  };
+
+  async componentDidMount() {
+    const response = await fetch('/api/v1/user/getUsers');
+    const body = await response.json();
+    this.setState({users: body});
+  }
+
+  render() {
+    const {users} = this.state;
+    return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <div className="App-intro">
+              <h2>Users</h2>
+              {users.map(client =>
+                  <div key={client.id}>
+                    {client.name} ({client.email})
+                  </div>
+              )}
+            </div>
+          </header>
+        </div>
+    );
+  }
 }
-
 export default App;
+
